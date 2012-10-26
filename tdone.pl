e9be8951c91925f1813d6146e66836e9b56b0f64
ben hengst <notbenh@cpan.org>
2012-10-25 20:43:15 -0700

    gave up going with remote file as its simple

--- tdone.pl
+++ tdone.pl
@@ -5,20 +5,30 @@ use Data::Dumper; sub D(@){ warn Dumper(@_) };
 use Tie::File;
 use v5.10;   
 
-tie my @tasks, 'Tie::File', $ENV{TDONE_FILE} || <main::DATA>; # ? or die?
+my @tasks;
+tie @tasks, 'Tie::File', $ENV{TDONE_FILE} or die qq{TDONE_FILE not specified in env: $! $@};
 
-my @actions = qw{add list done find};
+my @actions = qw{add list done find edit};
 my $action  = @ARGV == 0           ? 'list'
             : $ARGV[0] ~~ @actions ? shift
             :                        'add'
             ;
 
 given ($action) {
-  when ('list') { D {LIST => \@ARGV}}
-  when ('add' ) { D {ADD  => \@ARGV}}
-  when ('done') { D {DONE => \@ARGV}}
-  when ('find') { D {FIND => \@ARGV}}
+  when ('list') { 
+    my $fmt = sprintf qq{%% %ds: %%s\n}, length( scalar( @tasks ));
+    my $i;
+    printf $fmt, $i++, $_ for @tasks;
+  }
+  when ('add' ) { push @tasks, join ' ', @ARGV; }
+  when ('done') { 
+    delete $tasks[$_] for reverse sort @ARGV;
+  }
+  when ('find') { D {FIND => \@ARGV, TASKS => \@tasks}}
+  when ('edit') { exec $ENV{VISUAL} || $ENV{EDITOR}, $ENV{TDONE_FILE}; }
   default       { qx{perldoc $0}   } # USEAGE
 }
-__END__
-finish this thing @laptop
+
+# sort
+@tasks = grep{length} @tasks; # clean up any blank lines
+# write
